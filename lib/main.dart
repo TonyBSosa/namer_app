09886c746d76244246c44abaf7799b7b9f389b0f
TonyBSosa <tonyblad09@gmail.com>
2024-10-25 19:09:36 -0600

    Navigation Rail

--- lib/main.dart
+++ lib/main.dart
@@ -41,62 +41,118 @@ class MyAppState extends ChangeNotifier {
     }
     notifyListeners();
 }
+ }
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
-class MyHomePage extends StatelessWidget {
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+
+Widget page;
+switch (selectedIndex) {
+  case 0:
+    page = GeneratorPage();
+    break;
+  case 1:
+    page = Placeholder();
+    break;
+  default:
+    throw UnimplementedError('no widget for $selectedIndex');
+}
+
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                setState((){
+                                  selectedIndex = value;
+
+
+                });
+
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
- IconData icon;
+    IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
     } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-         
-                BigCard(pair:pair),
-                SizedBox(height: 10),
-                     Row(
-                      mainAxisSize: MainAxisSize.min,
-                       children: [
-                        
-                                      ElevatedButton.icon(
-                                       onPressed: () {
-                                         appState.toggleFavorite();  
-                                       },
-                                       icon: Icon(icon),
-                                       label: Text('like'),
-
-                          
-                                     ),
-                                     SizedBox(width:10),
-
-                                      ElevatedButton(
-                                       onPressed: () {
-                                         appState.getNext();  
-                                       },
-                                       child: Text('Next'   ),
-
-                                       
-                                       
-                                     
-                                     ),
-                       ],
-                     ),
-        
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
@@ -117,7 +173,7 @@ class BigCard extends StatelessWidget {
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
-        padding: const EdgeInsets.all(60),
+        padding: const EdgeInsets.all(40),
         child: Text(pair.asLowerCase, style: style,
         semanticsLabel: pair.asPascalCase,
         ),
